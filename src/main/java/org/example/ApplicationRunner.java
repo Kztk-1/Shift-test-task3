b90a37570dc5c20454e35330dd4838facbf68220
KoZZZtik-3d <kv.sobolev06@gmail.com>
2025-07-06 15:39:26 +0300

    feat: создан ApplicationRunner для запуска приложения
    Класс содержит все себе все основные компоненты программы: ArgsParser, DataFilterEngine, StatisticsPrinter.
    В нем реализуется их совместная работа

---
+++ src/main/java/org/example/ApplicationRunner.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import org.example.cli.ArgsParser;
+import org.example.dto.FilterConfig;
+import org.example.engine.DataFilterEngine;
+import org.example.model.TotalStats;
+import org.example.util.StatisticsPrinter;
+
+public class ApplicationRunner {
+    private final ArgsParser argsParser;
+    private final DataFilterEngine engine;
+    private final StatisticsPrinter statisticsPrinter;
+
+    // Цепочка конструкторов (тут вызвается конструктор для тестов и в него пихаются new-аргументы)
+    public ApplicationRunner() {
+        this(new ArgsParser(), new DataFilterEngine(), new StatisticsPrinter());
+    }
+
+    // Конструктор для тестов (внедрение зависимостей)
+    public ApplicationRunner(ArgsParser argsParser,
+                             DataFilterEngine engine,
+                             StatisticsPrinter statisticsPrinter) {
+        this.argsParser = argsParser;
+        this.engine = engine;
+        this.statisticsPrinter = statisticsPrinter;
+    }
+
+    public int run(String[] args) {
+        try {
+            // Получаем данные из args
+            FilterConfig config = argsParser.parse(args);
+            // Обработка файлов + сбор статистики + запись в новые файлы
+            engine.process(config);
+            // Получение собранной статистики
+            TotalStats stats = engine.getTotalStats();
+            // Вывод статистики
+            statisticsPrinter.print(stats, config.fullStats());
+            return 0;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Argument error: " + e.getMessage());
+            return 1;
+        } catch (Exception e) {
+            System.err.println("Processing error: " + e.getMessage());
+            e.printStackTrace();
+            return 2;
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ApplicationRunner app = new ApplicationRunner();
+        int exitCode = app.run(args);
+        System.exit(exitCode);
+
     }
 }
